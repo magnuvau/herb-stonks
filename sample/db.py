b347a204d2cf8eeac5571e9396dbd31add2213a4
magnuvau <austrheim.magnus@protonmail.com>
2021-02-20 13:36:50 +0100

    Move postgres to dedicated container

--- sample/db.py
+++ sample/db.py
@@ -1,12 +1,16 @@
 import psycopg2
 import time
 
-
 def connection():
     connection = None
     while connection is None:
         try:
-            connection = psycopg2.connect('dbname=postgres user=postgres')
+            connection = psycopg2.connect(
+                host='172.20.128.2',
+                database='postgres',
+                user='postgres',
+                password='postgres'
+            )
         except psycopg2.OperationalError:
             print("Failed to connect to postgres...")
             time.sleep(2)
--- sample/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting..." > /usr/src/app/log.txt
-
-python3 /usr/src/app/scrape.py
-
-echo "Done!" >> /usr/src/app/log.txt
