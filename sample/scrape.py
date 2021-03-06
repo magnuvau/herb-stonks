5c578ec2cb81e95ccc60be41910352f57271cf9a
magnuvau <austrheim.magnus@protonmail.com>
2021-03-06 12:57:28 +0100

    Add all herb types

--- sample/scrape.py
+++ sample/scrape.py
@@ -14,8 +14,34 @@ def log(msg):
         log.close()
 
 items = [
+    ("Guam seed", 5291),
+    ("Guam weed", 249),
+    ("Marrentill seed", 5292),
+    ("Marrentill weed", 251),
+    ("Tarromin seed", 5293),
+    ("Tarromin weed", 253),
+    ("Harralander seed", 5294),
+    ("Harralander weed", 255),
     ("Ranarr seed", 5295),
-    ("Ranarr weed", 257)
+    ("Ranarr weed", 257),
+    ("Toadflax seed", 5296),
+    ("Toadflax weed", 2998),
+    ("Irit seed", 5297),
+    ("Irit weed", 259),
+    ("Avantoe seed", 5298),
+    ("Avantoe weed", 261),
+    ("Kwuarm seed", 5299),
+    ("Kwuarm weed", 263),
+    ("Snapdragon seed", 5300),
+    ("Snapdragon weed", 3000),
+    ("Cadantine seed", 5301),
+    ("Cadantine weed", 265),
+    ("Lantadyme seed", 5302),
+    ("Lantadyme weed", 2481),
+    ("Dwarf seed", 5303),
+    ("Dwarf weed", 267),
+    ("Torstol seed", 5304),
+    ("Torstol weed", 269)
 ]
 
 api_url = Template('https://secure.runescape.com/m=itemdb_oldschool/api/graph/${ITEM_ID}.json')
